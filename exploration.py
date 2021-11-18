8e319c190ca545cea014e62f8f6143084569ceea
ethanglaser <glasere@purdue.edu>
2021-11-17 22:12:07 -0500

    ready for martin

--- exploration.py
+++ exploration.py
@@ -3,49 +3,54 @@ import matplotlib.pyplot as plt
 import datetime
 from collections import Counter
 
-df = pd.read_csv('Data/202107-citibike-tripdata.csv')
-df = df[['ended_at', 'started_at', 'start_station_id', 'end_station_id']]
-df['ended_at'] = pd.to_datetime(df['ended_at'])
-df['started_at'] = pd.to_datetime(df['started_at'])
-#df['weekday'] = df['started_at'].dt.weekday
-df = df[(df['started_at'].dt.weekday != 5) & (df['started_at'].dt.weekday != 6)]
-df = df[(df['start_station_id'] == 5980.07) | (df['end_station_id'] == 5980.07)]
 
-df_start = df[df['start_station_id'] == 5980.07]
-df_end = df[df['end_station_id'] == 5980.07]
-df_start['result'] = -1
-df_end['result'] = 1
-df_start['time'] = df_start['started_at']
-df_end['time'] = df_end['ended_at']
-df_start = df_start[['result', 'time']]
-df_end = df_end[['result', 'time']]
-df_all = pd.concat([df_start, df_end])
+def df_prep(df):
+    df = df[['ended_at', 'started_at', 'start_station_id', 'end_station_id']]
+    df['ended_at'] = pd.to_datetime(df['ended_at'])
+    df['started_at'] = pd.to_datetime(df['started_at'])
+    df = df[(df['started_at'].dt.weekday != 5) & (df['started_at'].dt.weekday != 6)]
+    return df
 
-df_all = df_all.sort_values(by=['time'])
-results = df_all['result'].tolist()
-times = df_all['time'].tolist()
-current_day = ''
-min_index = 0
-x = []
-x_dict = {}
-for i in range(len(results)):
-    if times[i].day != current_day:
-        if current_day != '':
-            x_dict[current_day]['range'] = max(x_dict[current_day]['counts']) - min(x_dict[current_day]['counts'])
-            plt.figure()
-            plt.plot(times[min_index:i], x[min_index:i])
-            plt.savefig('Figures/Days/day' + str(current_day))
-        current_day = times[i].day
-        min_index = i
-        x_dict[current_day] = {'counts': [], 'times': [], 'events': [], 'range': 0}
-    x_dict[current_day]['counts'].append(sum(results[min_index:i+1]))
-    x_dict[current_day]['events'].append(results[i])
-    x_dict[current_day]['times'].append(times[i])
-    x.append(sum(results[min_index:i+1]))
-print([x_dict[key]['range'] for key in x_dict])
-plt.figure()
-plt.plot(times,x)
-plt.savefig('results2')
+def get_results_times(df, station_id):
+    df = df[(df['start_station_id'] == station_id) | (df['end_station_id'] == station_id)]
+
+    df_start = df[df['start_station_id'] == station_id]
+    df_end = df[df['end_station_id'] == station_id]
+    df_start['result'] = -1
+    df_end['result'] = 1
+    df_start['time'] = df_start['started_at']
+    df_end['time'] = df_end['ended_at']
+    df_start = df_start[['result', 'time']]
+    df_end = df_end[['result', 'time']]
+    df_all = pd.concat([df_start, df_end])
+
+    df_all = df_all.sort_values(by=['time'])
+    return df_all['result'].tolist(),  df_all['time'].tolist()
+
+def create_dict(results, times):
+    current_day = ''
+    min_index = 0
+    x = []
+    x_dict = {}
+    for i in range(len(results)):
+        if times[i].day != current_day:
+            if current_day != '':
+                x_dict[current_day]['range'] = max(x_dict[current_day]['counts']) - min(x_dict[current_day]['counts'])
+                plt.figure()
+                plt.plot(times[min_index:i], x[min_index:i])
+                plt.savefig('Figures/Days/day' + str(current_day))
+            current_day = times[i].day
+            min_index = i
+            x_dict[current_day] = {'counts': [], 'times': [], 'events': [], 'range': 0}
+        x_dict[current_day]['counts'].append(sum(results[min_index:i+1]))
+        x_dict[current_day]['events'].append(results[i])
+        x_dict[current_day]['times'].append(times[i])
+        x.append(sum(results[min_index:i+1]))
+    print([x_dict[key]['range'] for key in x_dict])
+    #plt.figure()
+    #plt.plot(times,x)
+    #plt.savefig('results2')
+    return x_dict
 
 def plot_everyday(start_hour, end_hour, data_dict):
     plt.figure()
@@ -74,12 +79,28 @@ def get_activity(x_dict, time_interval, start_hour, end_hour):
 
     return activity
 
-# plot_everyday(6, 10, x_dict)
-# plot_everyday(6, 11, x_dict)
-# plot_everyday(12, 20, x_dict)
-plot_everyday(17, 23, x_dict)
+'''
+Martin function here
+'''
 
-activity = get_activity(x_dict, 5, 17, 23)
-plt.figure()
-plt.hist(activity)
-plt.savefig('Figures/activity_hist')
+
+def create_station_markov(df, station_id, start_hour, end_hour, time_interval, capacity):
+    results, times = get_results_times(df, station_id)
+    my_dict = create_dict(results, times)
+    activity = get_activity(my_dict, time_interval, start_hour, end_hour)
+    # martin
+    # markov = martin_function(capacity, activity)
+    # return markov
+
+if __name__ == '__main__':
+    df = pd.read_csv('Data/202107-citibike-tripdata.csv')
+
+    station_id = 5980.07
+    capacity = 90
+    start_hour = 8
+    end_hour = 12
+    time_interval = 5
+    df = df_prep(df)
+    create_station_markov(df, station_id, start_hour, end_hour, time_interval, capacity)
+
+
